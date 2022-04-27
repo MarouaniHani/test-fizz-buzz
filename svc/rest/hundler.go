39ad78a63a64cea3ecbf234f860a9553efb0572f
Marouani Hani <marouanyhany7@gmail.com>
2022-04-27 16:02:42 +0100

    update generate api

--- svc/rest/hundler.go
+++ svc/rest/hundler.go
@@ -2,8 +2,14 @@ package rest
 
 import (
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"strings"
 	"test-fizz-buzz/dto"
 	"test-fizz-buzz/pkg"
+)
+
+var (
+	m = make(map[string]int)
 )
 
 func (s *Server) GenerateListOfStrings(c *gin.Context) {
@@ -17,6 +23,18 @@ func (s *Server) GenerateListOfStrings(c *gin.Context) {
 		BindJsonErr(c, err)
 		return
 	}
+
 	result := FizzBuzz(req.FirstString, req.SecondString, req.FirstNumber, req.SecondNumber, req.Limit)
 	ResponseData(c, result)
+
+	var params []string
+	params = append(params, req.FirstString, req.SecondString, strconv.Itoa(req.FirstNumber), strconv.Itoa(req.SecondNumber), strconv.Itoa(req.Limit))
+	res := strings.Join(params, ";")
+
+	val, ok := m[res]
+	if ok {
+		m[res] = val + 1
+	} else {
+		m[res] = 1
+	}
 }
